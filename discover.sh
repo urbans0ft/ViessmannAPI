97c459f5aa431db567812d617b1b735cb757c06b
urbanczykN <nico.urbanczyk@oediv.de>
2022-08-25 11:37:02 +0200

    discovery of current setup, setting

--- discover.sh
+++ discover.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-installationId=$(source ./get.sh "/iot/v1/equipment/installations")
-#installationId=$($(source ./get.sh "/iot/v1/equipment/installations") | jq -r '.data[].id')
-#gatewaySerial=$(source ./get.sh "/iot/v1/equipment/gateways" | jq -r '.data[].serial')
-#deviceId=$(source ./get.sh "/iot/v1/equipment/installations/$installtionId/gateways/$gatewaySerial/devices" | jq -r '.data[0].id')
+
+installationId=$(source ./get.sh "/iot/v1/equipment/installations" | jq -r '.data[].id')
+gatewaySerial=$(source ./get.sh "/iot/v1/equipment/gateways" | jq -r '.data[].serial')
+deviceId=$(source ./get.sh "/iot/v1/equipment/installations/$installationId/gateways/$gatewaySerial/devices" | jq -r '.data[0].id')
 
 echo "{" >$SETTING_JSON_FILE
 echo "    \"installationId\": \"$installationId\"," >>$SETTING_JSON_FILE
